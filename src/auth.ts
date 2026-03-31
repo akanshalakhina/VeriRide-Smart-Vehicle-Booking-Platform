976e491a0b47acb957e1f6f659f2a9ce84346986
Akansha Lakhina <akanshalakhina23@gmail.com>
2026-03-31 15:26:21 +0530

    updated providers

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@
 import { connect } from "http2";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google";
 import connectToDB from "./lib/db";
 import bcrypt from "bcryptjs";
 import User from "./models/user.model";
@@ -44,7 +45,44 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           email: user.email,
           role: user.role
         };
-      },
-    })
-  ],
-})
+        },
+      }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+    }),
+  ], //callbacks are used to control what happens when a user signs in or out, or when a session is created or updated. We can use callbacks to add custom logic to these events, such as checking if a user has the correct role to access a certain page or resource.
+  //cookies contain session id which is unique identifier for login session
+  //“Cookies store small data in browser, sessions store user data on server using a session ID, and tokens (JWT) store authentication data on the client for stateless authentication.”
+  callbacks: {
+    async jwt({ token, user }: { token: any; user: any }) {
+      token.name = user.name;
+      token.id = user.id;
+      token.email = user.email;
+      token.role = user.role;
+      
+      return token;
+    },
+    async session({ session, token }: { session: any; token: any }) {
+    if(session.user){
+      session.user.name = token.name;
+      session.user.email= token.email;
+      session.user.role= token.role;
+      session.user.id= token.id;
+    }
+      return session;
+    }
+  },
+  //session is accessed in frontend ..session tells which current user is logged in
+  pages:{
+    signIn:"/signin",
+    error:"/signin" //error page is also signin page
+    
+  },
+  session: {
+    strategy:"jwt", //use jwt for session management  
+    maxAge : 10 * 24 * 60 * 60, // 1 day in seconds
+  },
+  secret: process.env.NEXTAUTH_SECRET, //secret key for encrypting session data
+
+});
