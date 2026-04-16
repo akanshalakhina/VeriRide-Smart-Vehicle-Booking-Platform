3c597a5f0921bc814a5685a9bc341f00a54b95aa
Akansha Lakhina <akanshalakhina23@gmail.com>
2026-04-16 17:10:38 +0530

    updatee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
 import PublicHome from "@/components/PublicHome";
-import Image from "next/image";
 
 export default function Home() {
   return (
